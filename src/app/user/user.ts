a9ee38650268151561ff1fabc450f5675fd279da
Jesus Ramos <jramos1918@gmail.com>
2026-03-03 20:27:59 -0600

    condensed code in users.html

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar
+  }
 }
