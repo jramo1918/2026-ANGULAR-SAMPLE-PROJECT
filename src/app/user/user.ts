8b37340530bb17ca92845dea784519130ca99e61
Jesus Ramos <jramos1918@gmail.com>
2026-03-05 19:31:12 -0600

    signal use on users component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,13 +10,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
 
   onSelectUser () {
-    console.log("CLICKED!");
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
