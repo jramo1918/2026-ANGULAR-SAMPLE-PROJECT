397e9243cf77fd16e03b7eb365b186c1de249aca
Jesus Ramos <jramos1918@gmail.com>
2026-03-09 18:10:49 -0500

    configs for input

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -8,8 +8,8 @@ import { Component, Input } from '@angular/core';
 })
 export class User {
 
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
   get ImagePath() {
     return 'assets/users/' + this.avatar;
